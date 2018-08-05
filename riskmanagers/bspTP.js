55e257012a6a02a9f7808ba09b809ae185e393a4
Kayako <michael@m2medien.com>
2018-08-05 22:05:07 +0200

    example rm

---
+++ riskmanagers/bspTP.js
@@ -0,0 +1,14 @@
+class indicator
+{
+    constructor(input)
+    {
+        this.input = input;
+        this.output = "";
+    }
+
+    setupProfit(input)
+    {
+        output = input * 1.01;
+    }
+}
+module.exports = indicator;
